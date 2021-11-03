cf4e6d54217ef2b3e724e3340fcef460bcc504e6
balazsborsos <b.blazs97@gmail.com>
2021-11-03 12:44:47 +0100

    mnist sequential model and training added

---
+++ mnist_example.py
@@ -0,0 +1,73 @@
+import tensorflow
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.layers import Conv2D, Input, Dense, MaxPool2D, BatchNormalization, GlobalAvgPool2D
+from tensorflow.python.ops.gen_batch_ops import batch
+
+# Approaches
+# tensorflow.keras.Sequential
+model = tensorflow.keras.Sequential(
+    [
+        Input(shape=(28,28,1)),
+        Conv2D(32, (3,3), activation='relu'),
+        Conv2D(64, (3,3), activation='relu'),
+        MaxPool2D(),
+        BatchNormalization(),
+
+        Conv2D(128, (3,3), activation='relu'),
+        MaxPool2D(),
+        BatchNormalization(),
+
+        GlobalAvgPool2D(),
+        Dense(64, activation='relu'),
+        Dense(10, activation='softmax')
+    ]
+)
+
+
+# functional approach: func that returns a model
+# tensorflow.keras.Model inherit from this class and modify
+
+def display_some_examples(examples, labels):
+
+    plt.figure(figsize=(10,10))
+
+    for i in range(25):
+        idx = np.random.randint(0, examples.shape[0]-1)
+        img = examples[idx]
+        label = labels[idx]
+
+        plt.subplot(5, 5, i+1)
+        plt.title(str(label))
+        plt.tight_layout()
+        plt.imshow(img, cmap='gray')
+
+    plt.show()
+
+if __name__ == '__main__':
+
+    (x_train, y_train), (x_test, y_test) = tensorflow.keras.datasets.mnist.load_data()
+    print("x_train.shape = ", x_train.shape)
+    print("y_train.shape = ", y_train.shape)
+    print("x_test.shape = ", x_test.shape)
+    print("y_test.shape = ", y_test.shape)
+
+    #display_some_examples(x_train, y_train)
+
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+
+    x_train = np.expand_dims(x_train, axis=-1)
+    x_test = np.expand_dims(x_test, axis=-1)
+
+    #y_train = tensorflow.keras.utils.to_categorical(y_train, 10)  # this is to transform to one-hot encoded format
+    #y_test = tensorflow.keras.utils.to_categorical(y_test, 10)  # this is to transform to one-hot encoded format
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics='accuracy') # categorical_crossentropy needs one-hot 
+                                                                                                #encoding for the labels, sparse solves that
+
+    # model training
+    model.fit(x_train, y_train, batch_size = 64, epochs = 3, validation_split = 0.2)
+
+    # evaluation on test set
+    model.evaluate(x_test, y_test, batch_size = 64)
