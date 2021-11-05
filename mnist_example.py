6b728f3157ce914cf8581060f60af28af25f2319
balazsborsos <b.blazs97@gmail.com>
2021-11-05 22:03:06 +0100

    restructuring

--- mnist_example.py
+++ mnist_example.py
@@ -5,6 +5,9 @@ import numpy as np
 from tensorflow.keras.layers import Conv2D, Input, Dense, MaxPool2D, BatchNormalization, GlobalAvgPool2D
 from tensorflow.python.ops.gen_batch_ops import batch
 
+from dl_models import functional_model, MyCustomModel
+from utils import display_some_examples
+
 # Approaches
 # tensorflow.keras.Sequential
 # only used in tutorials
@@ -26,79 +29,6 @@ seq_model = tensorflow.keras.Sequential(
     ]
 )
 
-
-# functional approach: func that returns a model
-# this let's you have more flexibility, more recommended in real life
-# e.g with a for loop you can create many different types of models based on the arguments it recieves
-def functional_model():
-    my_input = Input(shape=(28,28,1))
-    x = Conv2D(32, (3,3), activation='relu')(my_input)
-    x = Conv2D(64, (3,3), activation='relu')(x)
-    x = MaxPool2D()(x)
-    x = BatchNormalization()(x)
-
-    x = Conv2D(128, (3,3), activation='relu')(x)
-    x = MaxPool2D()(x)
-    x = BatchNormalization()(x)
-
-    x = GlobalAvgPool2D()(x)
-    x = Dense(64, activation='relu')(x)
-    x = Dense(10, activation='softmax')(x)
-
-    model = tensorflow.keras.Model(inputs=my_input, outputs=x)
-
-    return model
-
-
-# tensorflow.keras.Model inherit from this class and modify
-# similar to pytorch, also let's you have more flexibility
-class MyCustomModel(tensorflow.keras.Model):
-    def __init__(self):
-        super().__init__()
-        
-        self.conv1 = Conv2D(32, (3,3), activation='relu')
-        self.conv2 = Conv2D(64, (3,3), activation='relu')
-        self.maxpool1 = MaxPool2D()
-        self.batchnorm1 = BatchNormalization()
-
-        self.conv3 = Conv2D(128, (3,3), activation='relu')
-        self.maxpool2 = MaxPool2D()
-        self.batchnorm2 = BatchNormalization()
-
-        self.globalavgpool1 = GlobalAvgPool2D()
-        self.dense1 = Dense(64, activation='relu')
-        self.dense2 = Dense(10, activation='softmax')
-
-    def call(self, my_input):
-        x = self.conv1(my_input)
-        x = self.conv2(x)
-        x = self.maxpool1(x)
-        x = self.batchnorm1(x)
-        x = self.conv3(x)
-        x = self.maxpool2(x)
-        x = self.batchnorm2(x)
-        x = self.globalavgpool1(x)
-        x = self.dense1(x)
-        x = self.dense2(x)
-
-        return x
-
-
-def display_some_examples(examples, labels):
-
-    plt.figure(figsize=(10,10))
-
-    for i in range(25):
-        idx = np.random.randint(0, examples.shape[0]-1)
-        img = examples[idx]
-        label = labels[idx]
-
-        plt.subplot(5, 5, i+1)
-        plt.title(str(label))
-        plt.tight_layout()
-        plt.imshow(img, cmap='gray')
-
-    plt.show()
 
 if __name__ == '__main__':
 
@@ -129,3 +59,4 @@ if __name__ == '__main__':
 
     # evaluation on test set
     model.evaluate(x_test, y_test, batch_size = 64)
+
