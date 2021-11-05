d25c41d6118f155247520cd513d7c21c90a94275
balazsborsos <b.blazs97@gmail.com>
2021-11-05 21:47:21 +0100

    functional and class model types added

--- mnist_example.py
+++ mnist_example.py
@@ -1,3 +1,4 @@
+from numpy.lib.function_base import select
 import tensorflow
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,7 +7,8 @@ from tensorflow.python.ops.gen_batch_ops import batch
 
 # Approaches
 # tensorflow.keras.Sequential
-model = tensorflow.keras.Sequential(
+# only used in tutorials
+seq_model = tensorflow.keras.Sequential(
     [
         Input(shape=(28,28,1)),
         Conv2D(32, (3,3), activation='relu'),
@@ -26,7 +28,61 @@ model = tensorflow.keras.Sequential(
 
 
 # functional approach: func that returns a model
+# this let's you have more flexibility, more recommended in real life
+# e.g with a for loop you can create many different types of models based on the arguments it recieves
+def functional_model():
+    my_input = Input(shape=(28,28,1))
+    x = Conv2D(32, (3,3), activation='relu')(my_input)
+    x = Conv2D(64, (3,3), activation='relu')(x)
+    x = MaxPool2D()(x)
+    x = BatchNormalization()(x)
+
+    x = Conv2D(128, (3,3), activation='relu')(x)
+    x = MaxPool2D()(x)
+    x = BatchNormalization()(x)
+
+    x = GlobalAvgPool2D()(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(10, activation='softmax')(x)
+
+    model = tensorflow.keras.Model(inputs=my_input, outputs=x)
+
+    return model
+
+
 # tensorflow.keras.Model inherit from this class and modify
+# similar to pytorch, also let's you have more flexibility
+class MyCustomModel(tensorflow.keras.Model):
+    def __init__(self):
+        super().__init__()
+        
+        self.conv1 = Conv2D(32, (3,3), activation='relu')
+        self.conv2 = Conv2D(64, (3,3), activation='relu')
+        self.maxpool1 = MaxPool2D()
+        self.batchnorm1 = BatchNormalization()
+
+        self.conv3 = Conv2D(128, (3,3), activation='relu')
+        self.maxpool2 = MaxPool2D()
+        self.batchnorm2 = BatchNormalization()
+
+        self.globalavgpool1 = GlobalAvgPool2D()
+        self.dense1 = Dense(64, activation='relu')
+        self.dense2 = Dense(10, activation='softmax')
+
+    def call(self, my_input):
+        x = self.conv1(my_input)
+        x = self.conv2(x)
+        x = self.maxpool1(x)
+        x = self.batchnorm1(x)
+        x = self.conv3(x)
+        x = self.maxpool2(x)
+        x = self.batchnorm2(x)
+        x = self.globalavgpool1(x)
+        x = self.dense1(x)
+        x = self.dense2(x)
+
+        return x
+
 
 def display_some_examples(examples, labels):
 
@@ -63,6 +119,8 @@ if __name__ == '__main__':
     #y_train = tensorflow.keras.utils.to_categorical(y_train, 10)  # this is to transform to one-hot encoded format
     #y_test = tensorflow.keras.utils.to_categorical(y_test, 10)  # this is to transform to one-hot encoded format
 
+    #model = functional_model()
+    model = MyCustomModel()
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics='accuracy') # categorical_crossentropy needs one-hot 
                                                                                                 #encoding for the labels, sparse solves that
 
